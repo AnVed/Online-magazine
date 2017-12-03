f8aa0ad9a8088d4aea22aba3ab4b7e76493d9bbd
Анастасия Ведь <Lazuritova@yandex.ru>
2017-12-03 23:09:21 +0300

    Second check

--- djangogirls/mysite/urls.py
+++ djangogirls/mysite/urls.py
@@ -1,27 +1,21 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from django.http import HttpResponse
-from anved.views import add, start, catalog, soft_toys, games, cart, toy, account_logout, home, account_profile, paypal_pay, paypal_success
-import social.apps.django_app.urls
-import paypal.standard.ipn.urls
+"""doogle URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from doogle.views import start, search
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url('', include(social.apps.django_app.urls, namespace='social')),
-    url(r'^add-to-cart/$', add, name='add'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', start),
-    url(r'^(?P<number>[0-9]+)/', toy),
-    url(r'^catalog/', catalog),
-    url(r'^soft_toys/', soft_toys),
-    url(r'^games/', games),
-    url(r'^cart/', cart),
-    url(r'^accounts/logout/$', account_logout, name='logout'),
-    url(r'^accounts/login/$', home, name='login'),
-    url(r'^accounts/profile/$', account_profile, name='profile'),
-    url(r'^payment/cart/$', paypal_pay, name='cart'),
-    url(r'^payment/success/$', paypal_success, name='success'),
-    url(r'^paypal/', include(paypal.standard.ipn.urls)),
+    path('/', start),
+    path('/search', search),
 ]
